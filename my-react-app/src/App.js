7780af22dc927645c08db2c4c988191ab084cf53
Alirezaa02 <saeb771@gmail.com>
2025-03-17 01:58:12 +1000

    setup-react

---
+++ my-react-app/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <h1>Welcome to <b>IKOB</b> website</h1>
+      <h3>
+🇮🇷 Connecting Iranians in Brisbane 🇦🇺 <br></br>
+🎉 Events • Meetups • Culture</h3>
+      <p>Coming Soon...</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
